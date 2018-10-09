14f593e36d6b1a77d74432ea8e2f969945c2d22c
GabyMarapi <gaby1017x@gmail.com>
2018-10-09 15:38:04 -0500

    Updates

---
+++ precache-manifest.a2785d8f2b0d6169227d03d863b5d1e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45d975ffd74dd0f719d3",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/runtime~main.45d975ff.js"
+  },
+  {
+    "revision": "a277447c940e102f62b3",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/main.a277447c.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "a277447c940e102f62b3",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/css/main.4c25e04a.chunk.css"
+  },
+  {
+    "revision": "69205dee68c88468316799d0223baa2b",
+    "url": "/lim-2018-01-lab-songs-frameworks/index.html"
+  }
+];
